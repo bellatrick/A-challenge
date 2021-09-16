bfdf844947499c369eacc820ee16143303cbfaa0
Busayo Samuel <busayosamuel2016@gmail.com>
2021-09-16 01:39:18 +0100

    selected html elements in js file, connected firebase database with the project and created a function for displaying star rating

---
+++ index.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelector(".show-modal");
+const productsList = document.querySelector(".productList");
+const productName = document.querySelector(".productName");
+const reviewsContainer = document.querySelector(".reviews");
+const container = document.querySelector(".container");
+const ratingDiv = document.querySelector(".ratingDiv");
+const reviewTextInput = document.querySelector(".review");
+const loading=document.querySelector('.loading')
+const error=document.querySelector('.error')
+const form = document.querySelector(".form");
+const averageRatingNumber = document.querySelector(".ratingNumber");
